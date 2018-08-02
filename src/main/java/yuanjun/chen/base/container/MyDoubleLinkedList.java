513736b002c30fdd8e97b0efdb3b24e0f929200e
kokomal <mattleblanc@live.cn>
2018-08-02 17:24:57 +0800

    双向链表的原地逆序，单向链表的原地逆序

---
+++ src/main/java/yuanjun/chen/base/container/MyDoubleLinkedList.java
@@ -0,0 +1,131 @@
+package yuanjun.chen.base.container;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 双向链表
+ */
+public class MyDoubleLinkedList<T extends Object> {
+
+    private static final Logger logger = LogManager.getLogger(MyDoubleLinkedList.class);
+
+    private Node<T> dummy = new Node<T>(null, null, null);
+    // private Node<T> tail = null; // dummyHead+头插法可以完全无视tail或者head指针
+
+    public MyDoubleLinkedList() {
+        super();
+        dummy.next = dummy; // 需要先new然后知道自己地址，再将next和pre指向自己
+        dummy.pre = dummy;
+    }
+
+    public boolean isEmpty() {
+        return dummy.next == dummy;
+    }
+
+    public void insert(T item) {
+        Node<T> nd = new Node<T>(item, null, null);
+        nd.next = dummy.next;
+        nd.next.pre = nd;
+        dummy.next = nd;
+        nd.pre = dummy;
+    }
+
+    public Node<T> search(T item) {
+        Node<T> cur = dummy.next;
+        while (cur != dummy && !cur.val.equals(item)) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+    public void remove(T item) {
+        if (isEmpty())
+            return;
+        Node<T> cur = dummy.next;
+        while (cur != dummy && !cur.val.equals(item)) {
+            cur = cur.next;
+        }
+        if (item.equals(cur.val)) {
+            cur.pre.next = cur.next;
+            cur.next.pre = cur.pre;
+        }
+    }
+
+    // 头插法，遍历则需要逆序，略别扭
+    public void showAll() {
+        Node<T> cur = dummy.pre;
+        String str;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        if (isEmpty()) {
+            sb.append("]");
+            str = sb.toString();
+        } else {
+            while (cur != dummy) {
+                sb.append(cur.val + ", ");
+                cur = cur.pre;
+            }
+            str = sb.substring(0, sb.length() - 2);
+            str += "]";
+        }
+        logger.info("outputdata is = " + str);
+    }
+
+    public void showAllReverse() {
+        Node<T> cur = dummy.next;
+        String str;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        if (isEmpty()) {
+            sb.append("]");
+            str = sb.toString();
+        } else {
+            while (cur != dummy) {
+                sb.append(cur.val + ", ");
+                cur = cur.next;
+            }
+            str = sb.substring(0, sb.length() - 2);
+            str += "]";
+        }
+        logger.info("outputdata is = " + str);
+    }
+
+    public void inplaceReverse() {
+        if (isEmpty() || dummy.next == dummy.pre)
+            return;
+        Node<T> cur = dummy.next; // 首元素
+        Node<T> tail = dummy.pre;
+        Node<T> newtail = dummy;
+        while (cur != tail) {
+            Node<T> tmp = cur.next; // 保护首元素的下一个节点
+            cur.next = newtail; // 逐次把首元素移到尾部
+            newtail.pre = cur; // 重新指向pre和next
+            newtail = cur;
+            cur = tmp;
+        }
+        cur.next = newtail;
+        newtail.pre = cur;
+        dummy.next = cur;
+        cur.pre = dummy;
+    }
+
+    public static void main(String[] args) {
+        MyDoubleLinkedList<Integer> myll = new MyDoubleLinkedList<>();
+        myll.showAll();
+        myll.insert(11);
+        myll.showAll();
+        myll.insert(22);
+        myll.showAll();
+        myll.insert(33);
+        myll.showAll();
+        myll.insert(44);
+        myll.showAll();
+        myll.remove(44);
+        myll.showAll(); // 11, 33, 44
+        myll.showAllReverse(); // 44, 33, 11
+        myll.inplaceReverse(); // 开始本地逆转
+        myll.showAll(); // 44, 33, 11
+        myll.showAllReverse(); // 11, 33, 44
+    }
+}
---
+++ src/main/java/yuanjun/chen/base/container/MySinglyLinkedList.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package yuanjun.chen.base.container;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 单链表
+ *
+ */
+public class MySinglyLinkedList<T extends Object> {
+
+	private static final Logger logger = LogManager.getLogger(MySinglyLinkedList.class);
+
+	private Node<T> head;
+	private Node<T> tail;
+
+	public MySinglyLinkedList() {
+		super();
+		head = null;
+		tail = null;
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public void insert(T item) {
+		Node<T> nd = new Node<T>(item, null, null);
+		if (isEmpty()) {
+		    head = nd;
+		    tail = nd;
+		} else {
+		    tail.next = nd;
+		    tail = nd;
+		}
+	}
+
+	public Node<T> search(T item) {
+		Node<T> cur = head;
+		while (cur != null && cur.val != item) {
+			cur = cur.next;
+		}
+		return cur;
+	}
+
+	public void remove(T item) {
+		if (isEmpty())
+			return;
+		Node<T> cur = head;
+		if(cur.val.equals(item)) {
+		    this.head = head.next;
+		    return;
+		}
+		while (cur != null && cur.next != null && !cur.next.val.equals(item)) {
+			cur = cur.next;
+		}
+		if (cur.next == null) return;
+		cur.next = cur.next.next;
+		if (cur.next == null) { // 注意要保护好tail防止被删了后空指针
+		    tail = cur;
+		}
+	}
+
+	public void showAll() {
+		Node<T> cur = head;
+		String str;
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		if (isEmpty()) {
+			sb.append("]");
+			str = sb.toString();
+		} else {
+			while (cur != null) {
+				sb.append(cur.val + ", ");
+				cur = cur.next;
+			}
+			str = sb.substring(0, sb.length() - 2);
+			str += "]";
+		}
+		logger.info("outputdata is = " + str);
+	}
+
+	/**
+	 * 单链表的原地翻转
+	 */
+	public void inplaceReverse() {
+	    if (isEmpty() || head == tail) return;
+	    Node<T> cur = head;
+	    Node<T> newtail = null;
+	    // System.out.println("tail=" + tail.val);
+	    while (cur != tail) {
+	        // System.out.println("cur=" + cur.val);
+	        Node<T> tmp = cur.next;
+	        cur.next = newtail;
+	        newtail = cur;
+	        cur = tmp;
+	    }
+	    cur.next = newtail;
+	    head = cur;
+	}
+	
+	public static void main(String[] args) {
+		MySinglyLinkedList<Integer> myll = new MySinglyLinkedList<>();
+		myll.showAll();
+		myll.insert(11);
+		myll.showAll();
+		myll.insert(22);
+		myll.showAll();
+		myll.insert(33);
+		myll.showAll();
+		myll.insert(44);
+		myll.showAll();
+		myll.remove(22);
+		myll.showAll();
+		myll.inplaceReverse();
+		myll.showAll();
+	}
+}
