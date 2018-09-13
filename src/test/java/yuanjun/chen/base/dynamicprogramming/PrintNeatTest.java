5b8afc45f1f4b8b8ccb6cd5b20fc02f98193cb87
kokomal <mattleblanc@live.cn>
2018-09-13 16:55:24 +0800

    微调

--- src/test/java/yuanjun/chen/base/dynamicprogramming/PrintNeatTest.java
+++ src/test/java/yuanjun/chen/base/dynamicprogramming/PrintNeatTest.java
@@ -33,7 +33,7 @@ public class PrintNeatTest {
     @Test
     public void testNeatHard() {
         int legalLen = 125; // 每行最多125格
-        String[] nations = RandomGenner.generateRandomStrings(100, legalLen); // 初始化100个长度为1-25的字符串
+        String[] nations = RandomGenner.generateRandomStrings(100, legalLen); // 初始化100个长度为[1-125]的字符串
         System.out.println("raw words = " + Arrays.toString(nations));
         PrintNeatlyAlgo.init(legalLen, nations); 
         int res = PrintNeatlyAlgo.neatly();
